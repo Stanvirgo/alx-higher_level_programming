c4f2591e1adc3f040d7fbc4a4e05214b287965f5
Stanvirgo <stanvirgo7@gmail.com>
2023-01-11 05:11:39 -0800

    Fix it

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    A function that replaces or adds
+    A function that replaces or adds
+    """
+    a_dictionary[key] = value
+    return a_dictionary
